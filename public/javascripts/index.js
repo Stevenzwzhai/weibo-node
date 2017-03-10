8e051b67225fc0df47e856f48a41f3bbb42d300c
leizixing <1435606838@qq.com>
2017-03-10 09:52:00 +0800

    2017年3月10日09:51:59

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,278 @@
 $(function(){
     refreshBlog();
     $("#lev_scroll_part").jScrollPane();
+    //初始化表情
+    initEmoji();
+    //图片上传框配置
+    projectfileoptions = {
+        uploadUrl: '/blogs/createImgByBlogId',
+        uploadAsync: true,
+        fileSizeGetter : true,
+        language : 'zh',
+        allowedPreviewTypes : [ 'image' ],
+        allowedFileExtensions : [ 'jpg', 'png', 'gif' ],
+        maxFileSize : 2000,
+        maxFileNum: 9,
+        showCaption:false,
+        showRemove:false,
+        showUpload:false,
+        layoutTemplates:{
+            preview: '<div class="file-preview {class}">\n' +
+            '    <div class="close fileinput-remove" onclick="hideFileInputDiv()">×</div>\n' +
+            '    <div class="{dropClass}">\n' +
+            '    <div class="file-preview-thumbnails">\n' +
+            '    </div>\n' +
+            '    <div class="clearfix"></div>' +
+            '    <div class="file-preview-status text-center text-success"></div>\n' +
+            '    <div class="kv-fileinput-error"></div>\n' +
+            '    </div>\n' +
+            '</div>',
+            footer: '<div class="file-thumbnail-footer">\n' +
+            '     {actions}\n' +
+            '</div>',
+            actions: '<div class="file-actions">\n' +
+            '    <div class="file-footer-buttons">\n' +
+            '         {delete} {zoom} {other}' +
+            '    </div>\n' +
+            '    {drag}\n' +
+            '    <div class="clearfix"></div>\n' +
+            '</div>'
+        },
+        uploadExtraData: function (previewId, index) {
+            var obj = {};
+            obj['blog_id'] = $("input#new_blog_id").val();
+            return obj;
+        }
+    }
+    // 图片上传框生成
+    $('input[class=projectfile]').each(function() {
+        var imageurl = $(this).attr("value");
+        if (imageurl) {
+            var op = $.extend({
+                initialPreview : [ // 预览图片的设置
+                    "<img src='" + imageurl + "' class='file-preview-image'>", ]
+            }, projectfileoptions);
+            $(this).fileinput(op);
+        } else {
+            $(this).fileinput(projectfileoptions);
+        }
+    });
+    attach_image_num = 0;
+    //图片上传选择回调事件
+    $('input#attach_image').on('fileloaded', function(event, file, previewId, index, reader) {
+        if($("#emoji_content").val() == ""){
+            $("#emoji_content").val($("#emoji_content").val() + '分享图片');
+        }
+        attach_image_num += 1;
+        $("#attach_image_prompt").html("共"+attach_image_num+"张,还能上传"+(9-attach_image_num)+"张");
+    }).on('filebatchuploadcomplete', function(event, files, extra) {
+        //上传成功后执行事件
+        $(".close.fileinput-remove").click();
+        toastr.success('发布成功!');
+        $("#emoji_content").val("");
+        refreshBlog();
+    });
+});
+
+/**
+ * 发表博客
+ */
+function storeBlog() {
+    if($("#emoji_content").val() == ""){
+        toastr.error("请输入内容后再点发布");
+    }else{
+        //生成新的blog对象
+        $.ajax({
+            url: '/blogs/new',
+            type: 'GET',
+            async: true,
+            success: function (e) {
+                console.log(e);
+                $("input#new_blog_id").val(e.blog_id);
+                //保存blog到库
+                var data1 = $("input#new_blog_id").val();
+                var data2 = $("#emoji_content").val();
+                $.ajax({
+                    url: '/blogs/create',
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {blog_id:data1, content:data2},
+                    async: true,
+                    success: function (e) {
+                        console.log(e);
+                        $('#attach_image').fileinput('upload');
+                    },
+                    error: function (msg) {
+                        console.log(msg.responseText);
+                    }
+                });
+            },
+            error: function (msg) {
+                console.log(msg.responseText);
+            }
+        });
+    }
+}
+/**
+ * 删除博客
+ */
+function deleteBlog(e){
+    swal({
+            title: "",
+            text: "确认要删除这条微博吗？",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "确认",
+            cancelButtonText: '取消',
+            closeOnConfirm: true
+        },
+        function(){
+            $.ajax({
+                url: '/blogs/delete/' + e,
+                type: 'GET',
+                async: true,
+                success: function(e){
+                    console.log(e.success);
+                    if(e.success == 1){
+                        refreshBlog();
+                        toastr.success("删除成功");
+                    }
+                },
+                error: function(msg){
+                    console.log(msg.responseText);
+                }
+            });
+        });
+}
+//打开图片上传区域
+function showFileInputDiv(event){
+    if($(event.target).attr('type') == 'button'){
+        var f = $("#attach_btn").attr('data-bool');
+        if(f == 'false'){
+            $("#attach_btn").attr('data-bool','true');
+            $("#fileInputDiv").css('display','block');
+        }else{
+            $("#attach_btn").attr('data-bool','false');
+            $("#fileInputDiv").css('display','none');
+        }
+    }
+    $("#attach_image_prompt").html("共"+attach_image_num+"张,还能上传"+(9-attach_image_num)+"张");
+}
+//关闭图片上传区域
+function hideFileInputDiv(){
+    $("#attach_btn").attr('data-bool','false');
+    $("#fileInputDiv").css('display','none');
+    $("#emoji_content").val($("#emoji_content").val().replace('分享图片', ''));
+    attach_image_num = 0;
+}
+function refreshBlog() {
+    $.ajax({
+        url: '/blogs/all',
+        type: 'GET',
+        dataType: 'json',
+        async: true,
+        success: function (e) {
+            console.log(e);
+            $("#home").empty();
+            for(var i=e.length-1; i>=0; i--){
+                var html = $("#blog-panel-templet").html()
+                    .replace(/\{\$blog.id\$\}/g,e[i]._id)
+                    .replace('\{\$blog.user.avatar\$\}',e[i].user.avatar)
+                    .replace(/\{\$blog.user.name\$\}/g,e[i].user.name)
+                    .replace('\{\$blog.user.level\$\}',e[i].user.level)
+                    .replace('\{\$blog.created_atz\$\}',formatDate(e[i].created_at))
+                    .replace('\{\$blog.from\$\}',e[i].from)
+                    .replace('\{\$blog.content\$\}',e[i].content)
+                    .replace('\{\$blog.comments.length\$\}',e[i].comments.length)
+                    .replace('\{\$blog.praise\$\}',e[i].praise);
+                $("#home").append('<div id="'+e[i]._id+'" class="blog-panel panel panel-default">'+html+'</div>');
+                if(e[i].attach.length > 1){
+                    var html2 = '';
+                    for(j=0;j<e[i].attach.length;j++){
+                        html2 += $("#image_show_template_multi").html()
+                            .replace(/\{\$blog.attach.filename\$\}/g, e[i].attach[j].filename);
+                    }
+                    $("#" + e[i]._id).find('.WB_media').append(html2);
+                }else{
+                    var html2 = '';
+                    for(j=0;j<e[i].attach.length;j++){
+                        html2 += $("#image_show_template").html()
+                            .replace(/\{\$blog.attach.filename\$\}/g, e[i].attach[j].filename);
+                    }
+                    $("#" + e[i]._id).find('.WB_media').append(html2);
+                }
+            }
+            $(".blog-content").emojiParse({
+                icons: [{
+                    path: "dist/img/tieba/",
+                    file: ".jpg",
+                    placeholder: ":{alias}:",
+                    alias: {
+                        1: "hehe",
+                        2: "haha",
+                        3: "tushe",
+                        4: "a",
+                        5: "ku",
+                        6: "lu",
+                        7: "kaixin",
+                        8: "han",
+                        9: "lei",
+                        10: "heixian",
+                        11: "bishi",
+                        12: "bugaoxing",
+                        13: "zhenbang",
+                        14: "qian",
+                        15: "yiwen",
+                        16: "yinxian",
+                        17: "tu",
+                        18: "yi",
+                        19: "weiqu",
+                        20: "huaxin",
+                        21: "hu",
+                        22: "xiaonian",
+                        23: "neng",
+                        24: "taikaixin",
+                        25: "huaji",
+                        26: "mianqiang",
+                        27: "kuanghan",
+                        28: "guai",
+                        29: "shuijiao",
+                        30: "jinku",
+                        31: "shengqi",
+                        32: "jinya",
+                        33: "pen",
+                        34: "aixin",
+                        35: "xinsui",
+                        36: "meigui",
+                        37: "liwu",
+                        38: "caihong",
+                        39: "xxyl",
+                        40: "taiyang",
+                        41: "qianbi",
+                        42: "dnegpao",
+                        43: "chabei",
+                        44: "dangao",
+                        45: "yinyue",
+                        46: "haha2",
+                        47: "shenli",
+                        48: "damuzhi",
+                        49: "ruo",
+                        50: "OK"
+                    }
+                }, {
+                    path: "dist/img/qq/",
+                    file: ".gif",
+                    placeholder: "#qq_{alias}#"
+                }]
+            });
+        },
+        error: function (msg) {
+            console.log(msg.responseText);
+        }
+    });
+}
+function initEmoji(){
     $("#emoji_content").emoji({
         button: "#emoji_btn",
         showTab: true,
@@ -124,245 +396,16 @@ $(function(){
             placeholder: "#qq_{alias}#"
         }]
     });
-    //图片上传框配置
-    projectfileoptions = {
-        uploadUrl: '/blogs/create',
-        uploadAsync: true,
-        fileSizeGetter : true,
-        language : 'zh',
-        allowedPreviewTypes : [ 'image' ],
-        allowedFileExtensions : [ 'jpg', 'png', 'gif' ],
-        maxFileSize : 2000,
-        maxFileNum: 9,
-        showCaption:false,
-        showRemove:false,
-        showUpload:false,
-        layoutTemplates:{
-            preview: '<div class="file-preview {class}">\n' +
-            '    <div class="close fileinput-remove" onclick="hideFileInputDiv()">×</div>\n' +
-            '    <div class="{dropClass}">\n' +
-            '    <div class="file-preview-thumbnails">\n' +
-            '    </div>\n' +
-            '    <div class="clearfix"></div>' +
-            '    <div class="file-preview-status text-center text-success"></div>\n' +
-            '    <div class="kv-fileinput-error"></div>\n' +
-            '    </div>\n' +
-            '</div>',
-            footer: '<div class="file-thumbnail-footer">\n' +
-            '     {actions}\n' +
-            '</div>',
-            actions: '<div class="file-actions">\n' +
-            '    <div class="file-footer-buttons">\n' +
-            '         {delete} {zoom} {other}' +
-            '    </div>\n' +
-            '    {drag}\n' +
-            '    <div class="clearfix"></div>\n' +
-            '</div>'
-        },
-        uploadExtraData: function (previewId, index) {
-            var obj = {};
-            obj['content'] = $("#emoji_content").val();
-            return obj;
-        }
-    }
-
-    // 图片上传框生成
-    $('input[class=projectfile]').each(function() {
-        var imageurl = $(this).attr("value");
-
-        if (imageurl) {
-            var op = $.extend({
-                initialPreview : [ // 预览图片的设置
-                    "<img src='" + imageurl + "' class='file-preview-image'>", ]
-            }, projectfileoptions);
-
-            $(this).fileinput(op);
-        } else {
-            $(this).fileinput(projectfileoptions);
-        }
-    });
-    attach_image_num = 0;
-    //图片上传选择回调事件
-    $('input#attach_image').on('fileloaded', function(event, file, previewId, index, reader) {
-        if($("#emoji_content").val() == ""){
-            $("#emoji_content").val($("#emoji_content").val() + '分享图片');
-        }
-        attach_image_num += 1;
-        $("#attach_image_prompt").html("共"+attach_image_num+"张,还能上传"+(9-attach_image_num)+"张");
-    }).on('filebatchuploadcomplete', function(event, files, extra) {
-        //上传成功后执行事件
-        $(".close.fileinput-remove").click();
-        toastr.success('发布成功!');
-        $("#emoji_content").val("");
-        refreshBlog();
-    });
-});
-
-/**
- * 发表博客
- */
-function storeBlog() {
-    if($("#emoji_content").val() == ""){
-        toastr.error("请输入内容后再点发布");
-    }else{
-        $('#attach_image').fileinput('upload');
-        /*var data = $("#emoji_content").val();
-        $.ajax({
-            url: '/blogs/create',
-            type: 'POST',
-            dataType: 'json',
-            data: {content:data},
-            async: true,
-            success: function (e) {
-                console.log(e);
-                toastr.success('发布成功!');
-                $("#emoji_content").val("");
-                refreshBlog();
-            },
-            error: function (msg) {
-                console.log(msg.responseText);
-            }
-        });*/
-    }
 }
-/**
- * 删除博客
- */
-function deleteBlog(e){
-    swal({
-            title: "",
-            text: "确认要删除这条微博吗？",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "确认",
-            cancelButtonText: '取消',
-            closeOnConfirm: true
-        },
-        function(){
-            $.ajax({
-                url: '/blogs/delete/' + e,
-                type: 'GET',
-                async: true,
-                success: function(e){
-                    console.log(e.success);
-                    if(e.success == 1){
-                        refreshBlog();
-                        toastr.success("删除成功");
-                    }
-                },
-                error: function(msg){
-                    console.log(msg.responseText);
-                }
-            });
-        });
-}
-//打开图片上传区域
-function showFileInputDiv(event){
-    if($(event.target).attr('type') == 'button'){
-        var f = $("#attach_btn").attr('data-bool');
-        if(f == 'false'){
-            $("#attach_btn").attr('data-bool','true');
-            $("#fileInputDiv").css('display','block');
-        }else{
-            $("#attach_btn").attr('data-bool','false');
-            $("#fileInputDiv").css('display','none');
-        }
-    }
-    $("#attach_image_prompt").html("共"+attach_image_num+"张,还能上传"+(9-attach_image_num)+"张");
-}
-//关闭图片上传区域
-function hideFileInputDiv(){
-    $("#attach_btn").attr('data-bool','false');
-    $("#fileInputDiv").css('display','none');
-    $("#emoji_content").val($("#emoji_content").val().replace('分享图片', ''));
-    attach_image_num = 0;
-}
-function refreshBlog() {
+//初始化一个blog对象
+function initNewBlog(){
     $.ajax({
-        url: '/blogs/all',
+        url: '/blogs/new',
         type: 'GET',
-        dataType: 'json',
         async: true,
         success: function (e) {
             console.log(e);
-            $("#home").empty();
-            for(var i=e.length-1; i>=0; i--){
-                var html = $("#blog-panel-templet").html()
-                    .replace(/\{\$blog.id\$\}/g,e[i]._id)
-                    .replace('\{\$blog.user.avatar\$\}',e[i].user.avatar)
-                    .replace(/\{\$blog.user.name\$\}/g,e[i].user.name)
-                    .replace('\{\$blog.user.level\$\}',e[i].user.level)
-                    .replace('\{\$blog.created_atz\$\}',formatDate(e[i].created_at))
-                    .replace('\{\$blog.from\$\}',e[i].from)
-                    .replace('\{\$blog.content\$\}',e[i].content)
-                    .replace('\{\$blog.comments.length\$\}',e[i].comments.length)
-                    .replace('\{\$blog.praise\$\}',e[i].praise);
-                $("#home").append(html);
-            }
-            $(".blog-content").emojiParse({
-                icons: [{
-                    path: "dist/img/tieba/",
-                    file: ".jpg",
-                    placeholder: ":{alias}:",
-                    alias: {
-                        1: "hehe",
-                        2: "haha",
-                        3: "tushe",
-                        4: "a",
-                        5: "ku",
-                        6: "lu",
-                        7: "kaixin",
-                        8: "han",
-                        9: "lei",
-                        10: "heixian",
-                        11: "bishi",
-                        12: "bugaoxing",
-                        13: "zhenbang",
-                        14: "qian",
-                        15: "yiwen",
-                        16: "yinxian",
-                        17: "tu",
-                        18: "yi",
-                        19: "weiqu",
-                        20: "huaxin",
-                        21: "hu",
-                        22: "xiaonian",
-                        23: "neng",
-                        24: "taikaixin",
-                        25: "huaji",
-                        26: "mianqiang",
-                        27: "kuanghan",
-                        28: "guai",
-                        29: "shuijiao",
-                        30: "jinku",
-                        31: "shengqi",
-                        32: "jinya",
-                        33: "pen",
-                        34: "aixin",
-                        35: "xinsui",
-                        36: "meigui",
-                        37: "liwu",
-                        38: "caihong",
-                        39: "xxyl",
-                        40: "taiyang",
-                        41: "qianbi",
-                        42: "dnegpao",
-                        43: "chabei",
-                        44: "dangao",
-                        45: "yinyue",
-                        46: "haha2",
-                        47: "shenli",
-                        48: "damuzhi",
-                        49: "ruo",
-                        50: "OK"
-                    }
-                }, {
-                    path: "dist/img/qq/",
-                    file: ".gif",
-                    placeholder: "#qq_{alias}#"
-                }]
-            });
+            $("input#new_blog_id").val(e.blog_id);
         },
         error: function (msg) {
             console.log(msg.responseText);
--- routes/blogs.js
+++ routes/blogs.js
@@ -13,85 +13,69 @@ var crypto = require('crypto');
  * 展示所有博客
  */
 router.get('/all',function(req, res, next){
-    /*Blog.find()
+    Blog.find()
         .populate('user')
         .exec(function(err, blogs) {
             if(err) return handleError(err);
-            for(var i = 0; i < blogs.length; i ++){
-                var image_array = [];
-                UploadFile.find({'blog' : blogs[i]._id},function(err, uploadfiles){
-                    if(err) return handleError(err);
-                    image_array = uploadfiles;
-                })
-                blogs[i].attach = image_array
-                console.log(blogs[i].attach);
-            }
             res.json(blogs);
-        });*/
-    Blog.find().then(function(blog) {
-        UploadFile.find({'blog' : blogs[i]._id}).then(function(uploadfiles) {
-            res.json({
-                blog: blog,
-                uploadfiles: uploadfiles
-            });
         });
-    });
 });
 /**
  * 添加一个blog
  */
 router.post('/create', function(req, res, next){
-
+    var blog = new Blog({
+        blog_id:req.body.blog_id,
+        user:req.session.user_id,
+        from:req.session.from,
+        content:req.body.content,
+        praise:0
+    });
+    blog.save(function(err){
+        if(err) return handleError(err);
+        res.json(blog);
+    });
+});
+/**
+ * 添加blog附件
+ */
+router.post('/createImgByBlogId', function(req, res, next){
     //保存上传图片逻辑
     var form = new multiparty.Form();//实例一个multiparty
     form.uploadDir = "./public/uploadfiles/";//设置文件储存路径
     //开始解析前台传过来的文件
-    form.parse(req, function(err, fields, files) {
-        console.log(fields);
-        //先保存博客
-        var blog = new Blog({
-            user:req.session.user_id,
-            from:req.session.from,
-            content:fields.content[0],
-            praise:0
-        });
-        blog.save(function(err){
-            if(err) return handleError(err);
-            console.log(blog);
-            //保存上传图片
-            var filesTmp = JSON.stringify(files);
-            var pr = JSON.parse(filesTmp);
-            if(err){
-                console.log('parse error: ' + err);
-            }else{
-                for(var i = 0; i < pr.attach_image.length; i ++ ){
-                    var inputFile = files.attach_image[i];//获取第一个文件
-                    //console.log(inputFile);
-                    var finalname = inputFile.originalFilename;//获取文件名称
-                    //获取文件后缀
-                    var suffix = finalname.split('.').pop().toLowerCase();
-                    var new_path = "./public/uploadfiles/" + crypto.createHash('md5').update(finalname).digest('hex') + '.' + suffix;//获取文件名
-                    var old_path = inputFile.path;//获取文件原路径
-                    fs.renameSync(old_path, new_path);
-                    /**
-                     * 保存到uploadfile数据表
-                     */
-                    var uploadfile = new UploadFile({
-                        filename: crypto.createHash('md5').update(finalname).digest('hex') + '.' + suffix,
-                        filesize: inputFile.size,
-                        filetype: inputFile.headers['content-type'],
-                        created_at: new Date(),
-                        blog: blog._id
-                    });
-                    uploadfile.save(function(err, uploadfile){
+    form.parse(req, function(err, fields, files){
+        console.log('new blog'+fields.blog_id[0]);
+        var filesTmp = JSON.stringify(files);
+        var pr = JSON.parse(filesTmp);
+        if(err){
+            console.log('parse error: ' + err);
+        }else{
+            for(var i = 0; i < pr.attach_image.length; i ++ ){
+                var inputFile = files.attach_image[i];//获取第一个文件
+                var finalname = inputFile.originalFilename;//获取文件名称
+                //获取文件后缀
+                var suffix = finalname.split('.').pop().toLowerCase();
+                var new_path = "./public/uploadfiles/" + crypto.createHash('md5').update(finalname).digest('hex') + '.' + suffix;//获取文件名
+                var old_path = inputFile.path;//获取文件原路径
+                fs.renameSync(old_path, new_path);
+                var uploadfile = new UploadFile({
+                    filename: crypto.createHash('md5').update(finalname).digest('hex') + '.' + suffix,
+                    filesize: inputFile.size,
+                    filetype: inputFile.headers['content-type'],
+                    created_at: new Date()
+                });
+                uploadfile.save(function(err, uploadfile){
+                    if(err) return handleError(err);
+                    Blog.update({'blog_id':fields.blog_id[0]},{'$addToSet':{'attach':{'filename':uploadfile.filename}}},function(err, res){
                         if(err) return handleError(err);
-                        console.log(uploadfile);
+                        console.log(res);
                     });
-                }
+                });
             }
-        });
+        }
     });
-    res.send({success:1});
+    res.json({success:'图片上传成功'});
 });
 /**
  * 删除指定id博客
@@ -103,4 +87,18 @@ router.get('/delete/:id',function(req, res, next){
         res.json({success:1});
     });
 });
+/**
+ * 初始化新建博客
+ */
+router.get('/new', function(req, res, next){
+    var blog = new Blog({
+        blog_id: req.session.user_id + new Date().getTime() + 'blog',//生成博客的业务id
+        user:  req.session.user_id,
+        created_at: new Date(),
+        from: '微博weibo.com',
+        praise : 0
+    });
+    console.log(blog);
+    res.json(blog);
+})
 module.exports = router;
